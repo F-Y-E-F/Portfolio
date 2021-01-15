7f153d15b18d4e8c9c5c182c4d3ac9fee00e50ed
andryue <kacperwojak17@gmail.com>
2021-01-15 13:06:45 +0100

    add texts animations

--- portfolio/lib/screens/portfolio_screen.dart
+++ portfolio/lib/screens/portfolio_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 
 class PortfolioScreen extends StatefulWidget {
   static const routeName = '/';
@@ -10,6 +11,9 @@ class PortfolioScreen extends StatefulWidget {
 }
 
 class _PortfolioScreenState extends State<PortfolioScreen> {
+  int hovered = -1;
+  int clickedPortfolioType = 0;
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -29,11 +33,11 @@ class _PortfolioScreenState extends State<PortfolioScreen> {
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            _getMenuItem('HOME', theme),
-                            _getMenuItem('PORTFOLIO', theme),
-                            _getMenuItem('RESUME', theme),
-                            _getMenuItem('ABOUT', theme),
-                            _getMenuItem('CONTACT', theme)
+                            _getMenuItem('HOME', theme, 0),
+                            _getMenuItem('PORTFOLIO', theme, 1),
+                            _getMenuItem('RESUME', theme, 2),
+                            _getMenuItem('ABOUT', theme, 3),
+                            _getMenuItem('CONTACT', theme, 4)
                           ],
                         ),
                       ),
@@ -82,24 +86,82 @@ class _PortfolioScreenState extends State<PortfolioScreen> {
                         image:
                             const AssetImage('assets/images/placeholder.jpg'))),
               ),
+              const SizedBox(
+                height: 100,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Featured ",
+                    style: theme.textTheme.headline1
+                        .copyWith(fontWeight: FontWeight.w400, fontSize: 42.0),
+                  ),
+                  Text(
+                    "Portfolio",
+                    style: theme.textTheme.headline1
+                        .copyWith(fontSize: 42.0, color: theme.primaryColor),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _getPortfolioAppTypeText("All",theme,0),
+                  _getPortfolioAppTypeText("Kotlin",theme,1),
+                  _getPortfolioAppTypeText("Flutter",theme,2),
+                  _getPortfolioAppTypeText("Other",theme,3)
+                ],
+              )
             ],
           ),
         ));
   }
 
   //------------------- get to menu item ----------------------
-  Widget _getMenuItem(String title, ThemeData theme) => InkWell(
+  Widget _getMenuItem(String title, ThemeData theme, int position) => InkWell(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Text(
-            title,
+          child: AnimatedDefaultTextStyle(
+            duration: Duration(milliseconds: 200),
             style: theme.textTheme.headline5.copyWith(
                 letterSpacing: 1.0,
                 fontFamily: 'Lato',
-                fontWeight: FontWeight.w500),
+                fontWeight: FontWeight.w500,
+                color: hovered == position
+                    ? theme.primaryColor
+                    : Color(0xFFFFFFFF)),
+            child: Text(
+              title,
+            ),
           ),
         ),
         onTap: () => print('tap'),
-        onHover: (isHover) => print(isHover),
+        onHover: (isHover) {
+          setState(() => isHover ? hovered = position : hovered = -1);
+        },
       );
+
+  Widget _getPortfolioAppTypeText(String text,ThemeData theme, int clickedPosition)=>InkWell(
+    splashColor: Colors.transparent,
+    highlightColor:  Colors.transparent,
+    child: Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: AnimatedDefaultTextStyle(
+        duration: Duration(milliseconds: 200),
+        style: theme.textTheme.headline4.copyWith(
+          fontWeight: FontWeight.w400,
+            color: clickedPortfolioType == clickedPosition
+                ? theme.primaryColor
+                : Color(0xFFFFFFFF)),
+        child: Text(
+          text,
+        ),
+      ),
+    ),
+    onTap: () => setState(()=>clickedPortfolioType = clickedPosition)
+  );
 }
