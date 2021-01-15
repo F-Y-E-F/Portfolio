aa0b701bb3eb958ee4a3c37d73a9ca4b0b46429c
andryue <kacperwojak17@gmail.com>
2021-01-15 09:49:36 +0100

    add text theme in theme data

--- portfolio/lib/main.dart
+++ portfolio/lib/main.dart
@@ -13,7 +13,15 @@ class Portfolio extends StatelessWidget {
         fontFamily: 'OpenSans',
         primaryColor: Color(0xFFFCA311),
         backgroundColor: Color(0xFF0E0E0E),
-        accentColor: Color(0xFF202020)
+        accentColor: Color(0xFF202020),
+        textTheme: ThemeData.light().textTheme.copyWith(
+          headline1: TextStyle(fontSize: 36.0,color: Colors.white,fontWeight: FontWeight.w700),
+          headline2: TextStyle(fontSize: 30.0,color: Colors.white,fontWeight: FontWeight.w700),
+          headline3: TextStyle(fontSize: 24.0,color: Colors.white,fontWeight: FontWeight.w500),
+          headline4: TextStyle(fontSize: 20.0,color: Colors.white,fontWeight: FontWeight.w400),
+          headline5: TextStyle(fontSize: 16.0,color: Colors.white,fontWeight: FontWeight.w300),
+          headline6: TextStyle(fontSize: 14.0,color: Colors.white,fontWeight: FontWeight.w300)
+        ),
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -41,40 +49,22 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               'You have pushed the button this many times:',
+              style: Theme.of(context).textTheme.headline6
             ),
             Text(
               '$_counter',
@@ -90,7 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
