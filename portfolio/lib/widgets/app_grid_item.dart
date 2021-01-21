e6835a1e51e79968a373876787e782b9085c6ba4
andryue <kacperwojak17@gmail.com>
2021-01-21 12:32:45 +0100

    add hover action on app grid item

--- portfolio/lib/widgets/app_grid_item.dart
+++ portfolio/lib/widgets/app_grid_item.dart
@@ -1,52 +1,118 @@
 import 'package:flutter/material.dart';
 import '../models/app.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AppGridItem extends StatefulWidget {
   final App app;
+
   AppGridItem(this.app);
 
   @override
   _AppGridItemState createState() => _AppGridItemState();
 }
 
-class _AppGridItemState extends State<AppGridItem> with TickerProviderStateMixin {
-  AnimationController _controller;
-  Animation<double> _animation;
+class _AppGridItemState extends State<AppGridItem>
+    with TickerProviderStateMixin {
+  AnimationController _enterController;
+  Animation<double> _enterAnimation;
+
+  AnimationController _hoverController;
+  Animation<double> _hoverAnimation;
+
+  bool isHover = false;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
+    _enterController = AnimationController(
       duration: const Duration(milliseconds: 700),
       vsync: this,
     )..forward();
-    _animation = CurvedAnimation(
-      parent: _controller,
+    _enterAnimation = CurvedAnimation(
+      parent: _enterController,
       curve: Curves.fastOutSlowIn,
+    );
+
+    _hoverController = AnimationController(
+      duration: const Duration(milliseconds: 400),
+      vsync: this,
+    );
+    _hoverAnimation = CurvedAnimation(
+      parent: _hoverController,
+      curve: Curves.easeInOut,
     );
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _enterController.dispose();
     super.dispose();
   }
-  
-
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return ScaleTransition(
-      scale: _animation,
+      scale: _enterAnimation,
       child: Column(
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(3),
-            child: Image.network(
-              widget.app.iconUrl,
-              width: 320,
-              height: 320,
+          Container(
+            width: 320,
+            height: 320,
+            child: InkWell(
+              onTap: (){},
+              onHover: (isMouseHover) => isMouseHover ? _hoverController.forward() : _hoverController.reverse(),
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.center,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(3),
+                      child: Image.network(
+                        widget.app.iconUrl,
+                        width: 320,
+                        height: 320,
+                      ),
+                    ),
+                  ),
+                    FadeTransition(
+                      opacity: _hoverAnimation,
+                      child: Container(
+                        width: 320,
+                        height: 320,
+                        color: Colors.black54,
+                      ),
+                    ),
+                    FadeTransition(
+                      opacity: _hoverAnimation,
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: FlatButton(
+                          highlightColor: Colors.transparent,
+                          splashColor: Colors.transparent,
+                          hoverColor: Colors.white10,
+                          padding: EdgeInsets.symmetric(horizontal: 40,vertical: 20),
+                          onPressed: () => _launchURL('https://google.com'),
+                          child: Text(
+                            'Check this out',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                              letterSpacing: 0.8
+                            ),
+                          ),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(3),
+                              side: BorderSide(
+                                color: Colors.white,
+                                width: 2
+                              )),
+                        ),
+                      ),
+                    )
+                ],
+              ),
             ),
           ),
           const SizedBox(
@@ -67,5 +133,13 @@ class _AppGridItemState extends State<AppGridItem> with TickerProviderStateMixin
         ],
       ),
     );
+  }
+
+  Future<void> _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
